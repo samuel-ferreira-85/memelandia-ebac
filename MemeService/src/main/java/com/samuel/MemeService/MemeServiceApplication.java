5e921028f3e5779e60f26c0f7d6d9dad75663041
samuel-ferreira-85 <sam85avila@gmail.com>
2023-07-28 10:36:33 -0300

    Criação microsserviço de memes

---
+++ MemeService/src/main/java/com/samuel/MemeService/MemeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.MemeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeServiceApplication.class, args);
+	}
+
+}
---
+++ MemeService/src/test/java/com/samuel/MemeService/MemeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.MemeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
